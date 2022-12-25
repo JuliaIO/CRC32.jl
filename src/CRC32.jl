366e4ccae5fca38b93124be2126e0fff6b7846d8
Steven G. Johnson <stevenj@alum.mit.edu>
2022-12-25 10:04:19 -0500

    initial commit

--- src/CRC32.jl
+++ src/CRC32.jl
@@ -1,5 +1,94 @@
+# This file is a part of Julia. License is MIT: https://julialang.org/license
+
+"""
+Module for computing the CRC-32 checksum (ISO 3309, ITU-T V.42, CRC-32-IEEE),
+analogous to the Julia `CRC32c` standard-library module for CRC-32c.
+
+See [`CRC32.crc32`](@ref) for more information.
+"""
 module CRC32
 
-# Write your package code here.
+####################################################################
+# exported API, based on code in julia/stdlib/CRC32c/src/CRC32c.jl
+
+import Base.FastContiguousSubArray
+export crc32
+
+"""
+    crc32(data, crc::UInt32=0x00000000)
+
+Compute the CRC-32 checksum (ISO 3309, ITU-T V.42, CRC-32-IEEE) of the given `data`, which can be
+an `Array{UInt8}`, a contiguous subarray thereof, or a `String`.  Optionally, you can pass
+a starting `crc` integer to be mixed in with the checksum.  The `crc` parameter
+can be used to compute a checksum on data divided into chunks: performing
+`crc32(data2, crc32(data1))` is equivalent to the checksum of `[data1; data2]`.
+(Technically, a little-endian checksum is computed.)
+
+There is also a method `crc32(io, nb, crc)` to checksum `nb` bytes from
+a stream `io`, or `crc32(io, crc)` to checksum all the remaining bytes.
+Hence you can do [`open(crc32, filename)`](@ref) to checksum an entire file,
+or `crc32(seekstart(buf))` to checksum an [`IOBuffer`](@ref) without
+calling [`take!`](@ref).
+
+For a `String`, note that the result is specific to the UTF-8 encoding
+(a different checksum would be obtained from a different Unicode encoding).
+To checksum an `a::Array` of some other bitstype, you can do `crc32(reinterpret(UInt8,a))`,
+but note that the result may be endian-dependent.
+"""
+function crc32 end
+
+crc32(a::Union{Array{UInt8},FastContiguousSubArray{UInt8,N,<:Array{UInt8}} where N}, crc::UInt32=0x00000000) = _crc32(a, crc)
+crc32(s::Union{String, SubString{String}}, crc::UInt32=0x00000000) = _crc32(s, crc)
+
+"""
+    crc32(io::IO, [nb::Integer,] crc::UInt32=0x00000000)
+
+Read up to `nb` bytes from `io` and return the CRC-32 checksum, optionally
+mixed with a starting `crc` integer.  If `nb` is not supplied, then
+`io` will be read until the end of the stream.
+"""
+crc32(io::IO, nb::Integer, crc::UInt32=0x00000000) = _crc32(io, nb, crc)
+crc32(io::IO, crc::UInt32=0x00000000) = _crc32(io, crc)
+crc32(io::IOStream, crc::UInt32=0x00000000) = _crc32(io, crc)
+
+####################################################################
+# Low-level code, based on code from julia/base/util.jl but
+# using Zlib's crc32 function (which is standardized by LSB).
+
+import Zlib_jll: libz
+unsafe_crc32(a, n, crc) = ccall((:crc32, libz), Culong, (Culong, Ptr{UInt8}, Csize_t), crc, a, n) % UInt32
+
+_crc32(a::NTuple{<:Any, UInt8}, crc::UInt32=0x00000000) =
+    unsafe_crc32(Ref(a), length(a) % Csize_t, crc)
+_crc32(a::Union{Array{UInt8},FastContiguousSubArray{UInt8,N,<:Array{UInt8}} where N}, crc::UInt32=0x00000000) =
+    unsafe_crc32(a, length(a) % Csize_t, crc)
+
+function _crc32(s::Union{String, SubString{String}}, crc::UInt32=0x00000000)
+    unsafe_crc32(s, sizeof(s) % Csize_t, crc)
+end
+
+function _crc32(io::IO, nb::Integer, crc::UInt32=0x00000000)
+    nb < 0 && throw(ArgumentError("number of bytes to checksum must be ≥ 0, got $nb"))
+    # use block size 24576=8192*3, since that is the threshold for
+    # 3-way parallel SIMD code in the underlying jl_crc32 C function.
+    buf = Vector{UInt8}(undef, min(nb, 24576))
+    while !eof(io) && nb > 24576
+        n = readbytes!(io, buf)
+        crc = unsafe_crc32(buf, n % Csize_t, crc)
+        nb -= n
+    end
+    return unsafe_crc32(buf, readbytes!(io, buf, min(nb, length(buf))) % Csize_t, crc)
+end
+_crc32(io::IO, crc::UInt32=0x00000000) = _crc32(io, typemax(Int64), crc)
+_crc32(io::IOStream, crc::UInt32=0x00000000) = _crc32(io, filesize(io)-position(io), crc)
+
+_crc32(x::UInt64, crc::UInt32=0x00000000) =
+    ccall((:crc32, libz), Culong, (Culong, Ref{UInt64}, Csize_t), crc, x, 8) % UInt32
+
+# not sure why Base has these methods? skipping it to avoid UUID dep
+#_crc32(uuid::UUID, crc::UInt32=0x00000000) =
+#   ccall(:jl_crc32, UInt32, (UInt32, Ref{UInt128}, Csize_t), crc, uuid.value, 16)
+
+####################################################################
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,53 @@
 using CRC32
 using Test
 
+# based on julia/stdlib/CRC32/test/runtests.jl
+
 @testset "CRC32.jl" begin
-    # Write your tests here.
+    # CRC32 checksum (test data generated from @andrewcooke's CRC.jl package)
+    for (n,crc) in [(0, 0x00000000), (1, 0xa505df1b), (2, 0xb6cc4292), (3, 0x55bc801d), (4, 0xb63cfbcd), (5, 0x470b99f4), (6, 0x81f67724), (7, 0x70e46888), (8, 0x3fca88c5), (9, 0x40efab9e), (10, 0x2520577b), (11, 0x8222bee6), (12, 0x925fc655), (13, 0xb720698d), (14, 0xa6041d7e), (15, 0xf5a6aa3a), (16, 0x094c80f1), (17, 0x72014175), (18, 0x01a61a37), (19, 0xee00ad46), (20, 0x5789dff8), (21, 0x0ceef897), (22, 0x48b1b2c3), (23, 0x53f448f2), (24, 0x928e10a3), (25, 0xf9243b0f), (26, 0xbf262f5d), (27, 0x4d022d07), (28, 0x582a244c), (29, 0xce36a4cb), (30, 0x2475ff72), (31, 0xe1258797), (32, 0x87e6ec25), (33, 0xd5e8cd78), (34, 0x5969bfaa), (35, 0x463fbdb6), (36, 0xcc452258), (37, 0xfc7aa72e), (38, 0xdc251d18), (39, 0x64b8e7ad), (40, 0x4fb420c5), (41, 0x7bf1f5fe), (42, 0x53c708b5), (43, 0xc5e696c6), (44, 0x9218027d), (45, 0xb9fba67b), (46, 0xc9bab150), (47, 0x12713991), (48, 0x73c10dca), (49, 0xf81e0514), (50, 0x00f77475), (51, 0x4dbdfc5c), (52, 0x9126bb1b), (53, 0x0e45c4f9), (54, 0xd9d77568), (55, 0x290f749d), (56, 0x7497b79e), (57, 0x9ac44e71), (58, 0x3396b3db), (59, 0x723b9b46), (60, 0x62a04c34), (61, 0xabbcf765), (62, 0x2e10db06), (63, 0x8d29775e), (64, 0x2880fb99)]
+        s = String(UInt8[1:n;])
+        ss = SubString(String(UInt8[0:(n+1);]), 2:(n+1))
+        @test crc32(UInt8[1:n;]) == crc == crc32(s) == crc32(ss)
+    end
+
+    # test that crc parameter is equivalent to checksum of concatenated data,
+    # and test crc of subarrays:
+    a = UInt8[1:255;]
+    crc_256 = crc32(a)
+    @views for n = 1:255
+        @test crc32(a[n+1:end], crc32(a[1:n])) == crc_256
+    end
+    @test crc32(IOBuffer(a)) == crc_256
+    let buf = IOBuffer()
+        write(buf, a[1:3])
+        @test crc32(seekstart(buf)) == crc32(a[1:3])
+        @test crc32(buf) == 0x00000000
+        @test crc32(seek(buf, 1)) == crc32(a[2:3])
+        @test crc32(seek(buf, 0), 2) == crc32(a[1:2])
+        @test crc32(buf) == crc32(a[3:3])
+    end
+
+    let f = tempname()
+        try
+            write(f, a)
+            @test open(crc32, f) == crc_256
+            open(f, "r") do io
+                @test crc32(io, 16) == crc32(a[1:16])
+                @test crc32(io, 16) == crc32(a[17:32])
+                @test crc32(io) == crc32(a[33:end])
+                @test crc32(io, 1000) == 0x00000000
+            end
+            a = rand(UInt8, 30000)
+            write(f, a)
+            @test open(crc32, f) == crc32(a) == open(io -> crc32(io, 10^6), f)
+        finally
+            rm(f, force=true)
+        end
+    end
+
+    # more test data generated from @andrewcooke's CRC.jl package
+    for (s,crc) in [("m", 0xe101f268), ("265FK7ZaL4", 0xde12ba25), ("picosliZI4i8rje39N6aHwHJNC6anyvnEv9Fmrw7cqiiNexvagh05QyFJlikBFGUvZTpcvPOFqcYEegVfxx05918p8NBo366raIr", 0x075b8144), ("uKMNBMv0dKyjurd1IYsJ5E26wEgI7Cr0IxmxzoZK3B9VaKT1kaBOPT5wiKUIUmu5K9mdp16wUWwFtTmJe9hVsMSDyXocQz87C0VUvRWyMooUTcKUXJqNC3bs7On0fYvoVXadiKTJoampyBdIQQCo8HE5AXjdVlnduD2OMNt6Itv8xb3BrmzTJyISWvZ371zaXl0AXSEgSUPXzJrcjSa6fCwLGjdC7SlJv8UX4jG1WkF7dZw8H770dRywea318fsaGNkSwJXhYD0CI1pE6aKEUeInRQDSUVrmonZkg6nlRDfUQQSM0gOZb2VVmbpJYf24cFHGNjyHDFEeElrfmSYXPEXkk4CaZypa3lYAozMqFW18I6buetXN7HJR2nGMAhfFqUMb3KjEqsWn0i56OGZJefv0cLVez3yQLpdObG0MURqIqyHZk4o1kdzRai7M9z3o7Cmvbc2dVIQ9eT1XJ7QRSEMt62dOPlqkJs3th1e9pyOdEyiekavY1CYbJzmwke33XiGj1EQHsEDqYqBt0goIcSxd9vWFm6E1LPs5D4xPzFS7BiMtjhGnl3qcNx24qrVCK0RNo61q6NiRJiIbe39gt9g0WRrc8ylADPlXuyRznkgd78JnR6L7cFq8Mxpik1c682gr53M9GLNVw0wncsHsFW06PbqOeOHwhBhlLFYasbKYmY50snyV6BUnetLd8HZKg7eVJxvnZga2EvPbG8T7lL6jMMxivnC7gyPQ33UnH56l9usopsv7YYUe51mwMkfoo1kL5iG1bIxwt4PAhbAtVl29Rlzd9SYK6gxvzqazb2dpaoP8nm6wfNtJTf1CFH9fWo8tW6h37uqA9dVnhTZFu5JUulN2Ie7mjTw8XGNmmPUqMwVn2drOdoOpetQb4PowQcGQ3cDjmrTOhqyGhFb7uJewdZMozCnC9ZM1fXRFGg0BhGK7SoOodEenZV9ha2spynomj7kU3wBQCER7XNIi07HUibxzTYtZlJAUmuY2", 0x9b7ca9b1)]
+        @test crc32(s) == crc
+    end
 end
